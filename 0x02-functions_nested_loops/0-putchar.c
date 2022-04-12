1b986924ed597391bf51ea2343e227604d1eed92
Fanuel Tsegaye <t.fanuel13@gmail.com>
2022-04-12 21:18:44 +0300

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,14 @@
  */
 int main(void)
 {
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
-_putchar('_')
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
+_putchar('_');
 return (0);
 }
