f68003a667284cca533555847c9950e650f28c76
Fanuel Tsegaye <t.fanuel13@gmail.com>
2022-04-12 21:15:29 +0300

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * main-main block
+ * *Describtion:print -putchar in a new line
+ * Return:this program returns zero
+ */
+int main(void)
+{
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+_putchar('_')
+return (0);
+}
