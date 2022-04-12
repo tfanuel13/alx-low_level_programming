378b4f61e84f60432dcee8f66a0c02d76d2594b8
Fanuel Tsegaye <t.fanuel13@gmail.com>
2022-04-12 22:11:08 +0300

    task3

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,10 +6,10 @@
  *
  * Return: 1 if character is lowercase, 0 otherwise.
  */
-int _islower(int c )
+int _islower(int c)
 {
 if (c >= 'a' && c <= 'z')
-return(1);
+return (1);
 else
 return (0);
 }
