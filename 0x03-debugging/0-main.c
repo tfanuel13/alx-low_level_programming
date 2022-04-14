61997682726be0f1b6bb13c721a6bb5031536046
Fanuel Tsegaye <t.fanuel13@gmail.com>
2022-04-14 12:24:01 +0300

    task1

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,35 @@
-#include "main.h"
+<<<<<<< HEAD
+#include "holberton.h"
 
 /**
- * main - tests function that printss if integer is positive or negative
- * Retturn: zero
+ * main - tests function that prints if integer is positive or negative
+ * Return: 0
  */
+
 int main(void)
 {
 int i;
 
-i=0;
+i = 0;
 positive_or_negative(i);
 
 return (0);
+=======
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 0;
+        positive_or_negative(i);
+
+        return (0);
+>>>>>>> 6136273d5ca0f65630df7e14a088c9b0727a9e24
 }
 
